2f4892858dad852288e80abcdb2a4aaf14841634
bobmohsen <bobmohsen.com>
2023-11-25 12:06:33 +0300

    first commit

---
+++ my-bob/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './App.css';
+import Tea from "./test";
+function App() {
+  return (
+
+    <div>
+      
+      <Tea add="abanob" age="55"/>
+    </div>
+
+    );
+};
+
+export default App;
---
+++ my-bob/src/test.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+function tea({add,age}){
+    return(
+<div>
+    <h1>hello {function(){return age*age}} {age}</h1>
+    <h1>hello</h1>
+    <h1>hello {age}</h1>
+    <h1>hello</h1>
+    <h1>hello</h1>
+    <h1>hello</h1>
+    <h1>hello {age}</h1>
+<a href='./logo.svg'>addd</a>
+</div>
+        )
+}
+export default tea
